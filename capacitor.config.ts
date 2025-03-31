e6f5d9b4ee2065d45228fc973474cb9a741cf986
kevin Aparicio <aparicio_kevin@hotmail.com>
2025-03-30 23:29:03 -0600

    Correcion de Carpeta android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'FracX',
-  webDir: 'www'
+  webDir: 'www',
+
+  server: {
+    hostname: '192.168.1.200', // Dirección IP para desarrollo en red local
+    androidScheme: 'http'       // Protocolo para Android (usar https en producción)
+  }
 };
 
 export default config;
